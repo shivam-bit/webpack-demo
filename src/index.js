dc1f0afccd7f28cbc4b2e86c0e3fef3f2f0f8d02
shivam-bit <shivama5tronot@gmail.com>
2022-12-24 01:03:37 +0530

    feat: css and scss setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import "./styles/index.scss";
+
 const someRandomObject = {
-    "randomKey1":"randomValue1"
-}
+  randomKey1: "randomValue1",
+};
 
 const someRandomObject2 = {
-    ...someRandomObject,
-    "randomKey2":"randomValue2"
-}
+  ...someRandomObject,
+  randomKey2: "randomValue23",
+};
 
-console.log({someRandomObject,someRandomObject2});
+console.log({ someRandomObject, someRandomObject2 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require("path");
-const mode = process.env.NODE_ENV === 'production'?"production":"development"
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
   mode: mode,
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          miniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -14,10 +25,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [new miniCssExtractPlugin()],
   devtool: "source-map",
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
     },
+    hot: true,
   },
 };
